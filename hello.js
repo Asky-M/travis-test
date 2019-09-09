26d36c8a344966189d47041055aa31f09c5f759a
Asky M <asky25uchiha@gmail.com>
2019-09-10 02:24:37 +0700

    Update hello.js
    running on 0.0.0.0 port 8000

--- hello.js
+++ hello.js
@@ -5,4 +5,4 @@ http.createServer(function (req, res) {
   res.end('Welcome back mastah ||| work with travisCI NodeJS dan Github \n'); // missing semi-colon will fail the build
 }).listen(1337, '127.0.0.1');
  
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running at http://0.0.0.0:8000/');
