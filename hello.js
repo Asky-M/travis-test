4f38309bbc17eb67d71c41793c94ea15427900bb
asky <asky25uchiha@gmail.com>
2019-09-10 02:10:01 +0700

    first

---
+++ hello.js
@@ -0,0 +1,8 @@
+var http = require('http');
+ 
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Welcome back mastah ||| work with travisCI NodeJS dan Github \n'); // missing semi-colon will fail the build
+}).listen(1337, '127.0.0.1');
+ 
+console.log('Server running at http://127.0.0.1:1337/');
